db8de39209019452816d27955e0b8a6ce1a9a81e
Jacxk <tfermin1827@hotmail.com>
2019-12-09 02:54:12 -0500

    Completed day 9 chanllenge

---
+++ day9.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const [...input] = fs
+    .readFileSync("./input.txt", "utf-8")
+    .split(/,/g)
+    .map(a => parseInt(a));
+
+function solve(inputCode) {
+    function intCode() {
+        const copy = [...input];
+        let base = 0;
+        let diagnostic = -1;
+        for (let i = 0; copy.length; i++) {
+            let code = copy[i], codeString = String(code);
+            let param1 = 0, param2 = 0, param3 = 0, direction = code;
+            if (codeString.length > 2) {
+                let paramModes = codeString
+                    .substring(0, codeString.length - 2)
+                    .split("")
+                    .map(i => parseInt(i));
+                direction = parseInt(codeString.slice(-1));
+
+                if (paramModes.length > 0) {
+                    param1 = paramModes[paramModes.length - 1] || 0;
+                    param2 = paramModes[paramModes.length - 2] || 0;
+                    param3 = paramModes[paramModes.length - 3] || 0;
+                }
+            }
+
+            if (direction === 1 || direction === 2) {
+                i += 3;
+                let a = (param1 === 0 ? copy[copy[i - 2]] : param1 === 1 ? copy[i - 2] : copy[copy[i - 2] + base]) || 0,
+                    b = (param2 === 0 ? copy[copy[i - 1]] : param2 === 1 ? copy[i - 1] : copy[copy[i - 1] + base]) || 0,
+                    c = (param3 === 0 ? copy[i] : param3 === 1 ? i : copy[i] + base) || 0;
+
+                if (direction === 1) copy[c] = a + b;
+                else copy[c] = a * b;
+            } else if (direction === 3 || direction === 4) {
+                i++;
+                let c = (param1 === 0 ? copy[i] : param1 === 1 ? i : copy[i] + base) || 0;
+
+                if (direction === 3) copy[c] = inputCode;
+                else diagnostic = (param1 === 0 ? copy[copy[i]] : param1 === 1 ? copy[i] : copy[copy[i] + base]) || 0;
+            } else if (direction === 5 || direction === 6) {
+                i += 2;
+                let a = (param1 === 0 ? copy[copy[i - 1]] : param1 === 1 ? copy[i - 1] : copy[copy[i - 1] + base]) || 0,
+                    b = (param2 === 0 ? copy[copy[i]] : param2 === 1 ? copy[i] : copy[copy[i] + base]) || 0;
+
+                if (direction === 5 && a !== 0) i = b - 1;
+                else if (direction === 6 && a === 0) i = b - 1;
+            } else if (direction === 7 || direction === 8) {
+                i += 3;
+                let a = (param1 === 0 ? copy[copy[i - 2]] : param1 === 1 ? copy[i - 2] : copy[copy[i - 2] + base]) || 0,
+                    b = (param2 === 0 ? copy[copy[i - 1]] : param2 === 1 ? copy[i - 1] : copy[copy[i - 1] + base]) || 0,
+                    c = (param3 === 0 ? copy[i] : param3 === 1 ? i : copy[i] + base) || 0;
+
+                if (direction === 7) copy[c] = a < b ? 1 : 0;
+                else copy[c] = a === b ? 1 : 0;
+            } else if (direction === 9) {
+                i++;
+                base += (param1 === 0 ? copy[copy[i]] : param1 === 1 ? copy[i] : copy[copy[i] + base]) || 0;
+            } else if (direction === 99) break;
+        }
+        return diagnostic;
+    }
+
+    return intCode();
+}
+
+console.log(`Solution for part 1 is: ${solve(1)}`);
+console.log(`Solution for part 2 is: ${solve(2)}`);
